0159423e55a88c5cd8c6de45cd5a50283c134085
SaviourBassey <saviourbazi2021@yahoo.com>
2022-06-20 08:16:17 +0100

    removed unwanted importation

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
-from email import message
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 # Create your models here.
 
