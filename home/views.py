281772de4d5441993cad209babc756094e5101bc
SaviourBassey <saviourbazi2021@yahoo.com>
2022-06-20 09:54:39 +0100

    Used s3 bucket for storage

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from accounts.models import Key
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home/index.html")
+    key = Key.objects.all()[0]
+    return render(request, "home/index.html", {"key":key})
