850d45e540c223643165dd5cbb831e60f9fa17a1
SaviourBassey <saviourbazi2021@yahoo.com>
2022-06-21 11:08:11 +0100

    fixed an issue in the home view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from accounts.models import Key
+
 
 # Create your views here.
 
 def home(request):
-    key = Key.objects.all().order_by("-timestamp")[0]
-    return render(request, "home/index.html", {"key":key})
+    return render(request, "home/index.html"
+    )
